4129c483385d5b79999566ac7c057f916e4882fe
github username <github email address>
2019-03-04 14:02:44 -0500

    added show view page for users

--- app/models/painter.rb
+++ app/models/painter.rb
@@ -2,4 +2,14 @@ class Painter < ApplicationRecord
   has_many :reviews
   has_many :projects
   has_many :users, through: :projects
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :zip_code, presence: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :y_of_exp, presence: true
 end
